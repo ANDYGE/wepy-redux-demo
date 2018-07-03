8acef04d0e943b854a08b31b7df71e03e87d7d7d
geshaofei <geshaofei@126.com>
2018-07-04 06:36:23 +0800

    update

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,14 +1,14 @@
 import { handleActions } from 'redux-actions'
-import { INCREMENT, DECREMENT, ASYNC_INCREMENT } from '../types/counter'
+import { DECREMENT, ASYNC_INCREMENT } from '../types/counter'
 
 export default handleActions({
-  [INCREMENT] (state) {
+  INCREMENT (state) {
     return {
       ...state,
       num: state.num + 1
     }
   },
-  [DECREMENT] (state) {
+  [DECREMENT] (state) { // es6允许用变量名作为属性值，即放在方括号内
     return {
       ...state,
       num: state.num - 1
