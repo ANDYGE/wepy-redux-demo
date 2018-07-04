4258d469763ce8d8fe1899d7ef5296f1ae3dc0d9
geshaofei <geshaofei@126.com>
2018-07-04 08:35:03 +0800

    update

--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -1,7 +1,20 @@
 import { ASYNC_INCREMENT } from '../types/counter'
 import { createAction } from 'redux-actions'
 
-export const asyncInc = createAction(ASYNC_INCREMENT, () => {
+const sleep = s => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(1)
+    }, s * 1000)
+  })
+}
+
+export const asyncInc = createAction(ASYNC_INCREMENT, async () => {
+  const v = await sleep(1)
+  return v
+})
+
+export const asyncDes = createAction('ASYNC_DECREMENT', () => {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(1)
--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,26 +1,36 @@
 import { handleActions } from 'redux-actions'
 import { DECREMENT, ASYNC_INCREMENT } from '../types/counter'
 
-export default handleActions({
-  INCREMENT (state) {
-    return {
-      ...state,
-      num: state.num + 1
+export default handleActions(
+  {
+    INCREMENT(state) {
+      return {
+        ...state,
+        num: state.num + 1
+      }
+    },
+    [DECREMENT](state) {
+      // es6允许用变量名作为属性值，即放在方括号内
+      return {
+        ...state,
+        num: state.num - 1
+      }
+    },
+    [ASYNC_INCREMENT](state, action) {
+      return {
+        ...state,
+        asyncNum: state.asyncNum + action.payload
+      }
+    },
+    ASYNC_DECREMENT(state, action) {
+      return {
+        ...state,
+        asyncNum: state.asyncNum - action.payload
+      }
     }
   },
-  [DECREMENT] (state) { // es6允许用变量名作为属性值，即放在方括号内
-    return {
-      ...state,
-      num: state.num - 1
-    }
-  },
-  [ASYNC_INCREMENT] (state, action) {
-    return {
-      ...state,
-      asyncNum: state.asyncNum + action.payload
-    }
+  {
+    num: 0,
+    asyncNum: 0
   }
-}, {
-  num: 0,
-  asyncNum: 0
-})
+)
